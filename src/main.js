edc2ed296690db0160c3394fe442194dbee9f647
Volkov Nikita <crystalnik123@gmail.com>
2024-07-01 23:05:47 +0300

    debugging-1-done-1

---
+++ src/main.js
@@ -0,0 +1,29 @@
+const searchInput = document.querySelector('input');
+const searchButton = document.querySelector('.input-button');
+const closeButton = document.querySelector('.close');
+
+searchInput.addEventListener('keypress', logValueFromInput);
+searchInput.addEventListener('input', changeInputIcon);
+closeButton.addEventListener('click', clearInput);
+
+function logValueFromInput(event) {
+  if (event.key === 'Enter' && searchInput.value) {
+    console.log(searchInput.value);
+  }
+}
+
+function changeInputIcon() {
+  if (searchInput.value) {
+    searchButton.style.display = 'none';
+    closeButton.style.display = 'block';
+  } else {
+    searchButton.style.display = 'block';
+    closeButton.style.display = 'none';
+  }
+}
+
+function clearInput() {
+  searchInput.value = '';
+  searchButton.style.display = 'block';
+  closeButton.style.display = 'none';
+}
